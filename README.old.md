ad52e27cbff780e2aa335022e6e167d40e31fe7b
aaron <aaronlim314@gmail.com>
2021-10-19 20:35:16 +1100

    Added basic card layout and eventListener

--- README.old.md
+++
@@ -1,2 +0,0 @@
-# memory-card-game
-A memory card game where the player must select a card that has not been chosen yet.
--- src/App.js
+++ src/App.js
@@ -1,11 +1,44 @@
+import React, { useState, useEffect } from "react";
 import './App.css';
 
-function App() {
+const App = () => {
+  const [score, setScore] = useState(0);
+  const [cards, setcards] = useState(['あ',　'い',　'う',　'え',　'お',　'か',　'き', 'く', 'け', 'こ']);
+  const [picked, setPicked] = useState([]);
+
+  // equivalent to componentDidMount lifecycle method
+  useEffect(() => {
+    const selectItem = (e) => {
+      const key = e.target.key;
+      console.log('key is ' + key);
+      console.log(cards[key]);
+      console.log(e.key);
+      console.log(e['data-key']);
+      //cards[key].concat(picked); 
+    }; 
+    const cardSelector = document.querySelectorAll('.card');
+    cardSelector.forEach((card) => window.addEventListener('click', selectItem));
+
+    }, []);
+
   return (
-    <div>
-      
+    <div id='container'>
+      <header>Memory Card Game</header>
+      {returnList(cards)}
+      <footer>Made by Aaron L.</footer>
     </div>
   );
 }
 
+function returnList(listName) {
+  if(listName.length === 0) return;
+  
+  return (
+    <ul id='cards-list'>
+      {listName.map((item, index) => {
+        return <div key={index}><li key={index} data-key={index} className='card'>{item}</li></div>
+      })}
+    </ul>
+  )
+}
 export default App;
