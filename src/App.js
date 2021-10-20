a7d27330011c339efc52feb6437c3dabc09538ef
aaron <aaronlim314@gmail.com>
2021-10-20 12:15:07 +1100

    Added randomisation of card orders

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import './App.css';
 
 const App = () => {
   const [score, setScore] = useState(0);
-  const [cards, setcards] = useState(['あ',　'い',　'う',　'え',　'お',　'か',　'き', 'く', 'け', 'こ']);
-  const [picked, setPicked] = useState([]);
+  const [cards, setCards] = useState(['あ',　'い',　'う',　'え',　'お',　'か',　'き', 'く', 'け', 'こ']);
+  const [picked, setPicked] = useState(undefined);
+  const [displayedCards, setDisplayedCards] = useState(undefined);
 
-  // equivalent to componentDidMount lifecycle method
-  useEffect(() => {
-    const selectItem = (e) => {
-      const key = e.target.key;
-      console.log('key is ' + key);
-      console.log(cards[key]);
-      console.log(e.key);
-      console.log(e['data-key']);
-      //cards[key].concat(picked); 
-    }; 
+  function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      let temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+    }
+  }
+
+  const selectItem = (e) => {
+      const chosen = e.target.innerHTML;
+      const key = e.target.getAttribute('data-key');
+      if(picked === undefined) {
+        console.log('picked is undef');
+        setPicked(chosen);
+      } else if (picked.includes(chosen)) {
+        console.log('has been picked');
+      } else {
+        setPicked(picked.concat(chosen));
+        setScore(score + 1);
+      }
+      console.log('clicked: ' + chosen + ' | Picked: ' + picked)
+      console.log('-----------');
+  }; 
+
+  useEffect(() => {    
+    let arrayCopy = [...cards];
+    shuffleArray(arrayCopy);
+    setCards(arrayCopy);
+
     const cardSelector = document.querySelectorAll('.card');
-    cardSelector.forEach((card) => window.addEventListener('click', selectItem));
+    cardSelector.forEach((card) => card.addEventListener('click', selectItem));
 
-    }, []);
+    return () => {
+      const cardSelector = document.querySelectorAll('.card');
+      cardSelector.forEach((card) => card.removeEventListener('click', selectItem));      
+    };
+
+  }, [picked]);
+
 
   return (
     <div id='container'>
@@ -31,12 +58,12 @@ const App = () => {
 }
 
 function returnList(listName) {
-  if(listName.length === 0) return;
+  if(listName.length === 0 || undefined) return null;
   
   return (
     <ul id='cards-list'>
       {listName.map((item, index) => {
-        return <div key={index}><li key={index} data-key={index} className='card'>{item}</li></div>
+        return <div key={index}><li key={index} className='card'>{item}</li></div>
       })}
     </ul>
   )
